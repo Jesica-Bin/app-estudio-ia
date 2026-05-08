f7cb11fa2ccc91c4954e5d5e4919e09bc5021ae7
Jesica-Bin <jesicabin60@gmail.com>
2026-05-08 15:42:11 -0300

    MAL

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./src/routes');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/api', routes);
+const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
